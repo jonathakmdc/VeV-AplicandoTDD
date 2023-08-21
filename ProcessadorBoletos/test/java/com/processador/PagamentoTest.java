7978316468fde4681df1f6cd2cd718162b9f36d6
GustavoNeery <gustavoalvesneryy@hotmail.com>
2023-08-21 19:52:02 -0300

    inicia testes para pagamentos

---
+++ ProcessadorBoletos/test/java/com/processador/PagamentoTest.java
@@ -0,0 +1,24 @@
+package ProcessadorBoletos.test.java.com.processador;
+
+import ProcessadorBoletos.src.main.java.com.processador.Boleto;
+import ProcessadorBoletos.src.main.java.com.processador.Fatura;
+import ProcessadorBoletos.src.main.java.com.processador.ProcessadorDeBoletos;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class PagamentoTest {
+
+    @Test
+    void testCriacao() {
+        Pagamento pagamento = new Pagamento(1500.00, LocalDate.now(),"BOLETO");
+
+        assertEquals(1500.0, pagamento.getValorTotal(), 2);
+        assertEquals(LocalDate.now(), pagamento.getData());
+        assertEquals("Boleto", pagamento.getNomeCliente());
+
+    }
+}
