e93da8cc73d4bd2963dad01e1ee0a475726ca329
jonathakmdc <jonatha.kennedy.costa@ccc.ufcg.edu.br>
2023-08-21 00:06:21 -0300

    adição de teste para exclusão de tarefas no gerenciador

--- GerenciadorTarefas/src/test/java/com/gerenciador/GerenciadorDeTarefasTest.java
+++ GerenciadorTarefas/src/test/java/com/gerenciador/GerenciadorDeTarefasTest.java
@@ -41,5 +41,28 @@ class GerenciadorDeTarefasTest {
 		
 		assertEquals(null, gerenciador.editarTarefa(1, "Fazer Atividade 2", "realizar atividade de cálculo", LocalDate.of(2023, 8, 15), "baixa"));
 	}
+	
+	@Test
+	void testExclusaoTarefa() {
+		GerenciadorDeTarefas gerenciador = new GerenciadorDeTarefas();
+		assertEquals(0, gerenciador.getQuantidadeTarefas());
+		
+		gerenciador.addTarefa("Fazer Atividade 2", "realizar atividade de cálculo", LocalDate.of(2023, 8, 15), "baixa");
+		
+		assertEquals("Fazer Atividade 2", gerenciador.getTarefa(0).getTitulo());
+		assertEquals("realizar atividade de cálculo", gerenciador.getTarefa(0).getDescricao());
+		assertEquals(LocalDate.of(2023, 8, 15), gerenciador.getTarefa(0).getDataDeVencimento());
+		assertEquals("baixa", gerenciador.getTarefa(0).getPrioridade());
+		
+		assertEquals(1, gerenciador.getQuantidadeTarefas());
+		
+		assertTrue(gerenciador.excluirTarefa(0));
+		
+		assertEquals(0, gerenciador.getQuantidadeTarefas());
+		
+		assertEquals(null, gerenciador.getTarefa(0));
+		
+		assertFalse(gerenciador.excluirTarefa(0));
+	}
 
 }
