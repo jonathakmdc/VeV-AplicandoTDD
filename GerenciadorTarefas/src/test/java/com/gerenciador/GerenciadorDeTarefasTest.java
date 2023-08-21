b9a16d957746dff4f4efabd9850f534084bd87b4
jonathakmdc <jonatha.kennedy.costa@ccc.ufcg.edu.br>
2023-08-20 23:35:50 -0300

    ajuste no teste para edição de tarefas no gerenciador

--- GerenciadorTarefas/src/test/java/com/gerenciador/GerenciadorDeTarefasTest.java
+++ GerenciadorTarefas/src/test/java/com/gerenciador/GerenciadorDeTarefasTest.java
@@ -30,7 +30,12 @@ class GerenciadorDeTarefasTest {
 		
 		assertEquals(1, gerenciador.getQuantidadeTarefas());
 		
-		gerenciador.editarTarefa(0, "Fazer Atividade", "realizar atividade", LocalDate.of(2023, 8, 1), "media");
+		gerenciador.editarTarefa(0, "Fazer Atividade 2", "realizar atividade de cálculo", LocalDate.of(2023, 8, 15), "baixa");
+		
+		assertEquals("Fazer Atividade 2", gerenciador.getTarefa(0).getTitulo());
+		assertEquals("realizar atividade de cálculo", gerenciador.getTarefa(0).getDescricao());
+		assertEquals(LocalDate.of(2023, 8, 15), gerenciador.getTarefa(0).getDataDeVencimento());
+		assertEquals("baixa", gerenciador.getTarefa(0).getPrioridade());
 	}
 
 }
