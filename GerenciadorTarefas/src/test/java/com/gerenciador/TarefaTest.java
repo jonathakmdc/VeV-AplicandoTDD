ff81119a9e2ba7b821eabb52e0f46a66fd94ad4b
jonathakmdc <jonatha.kennedy.costa@ccc.ufcg.edu.br>
2023-08-20 22:28:37 -0300

    adição de teste para edição de tarefa

--- GerenciadorTarefas/src/test/java/com/gerenciador/TarefaTest.java
+++ GerenciadorTarefas/src/test/java/com/gerenciador/TarefaTest.java
@@ -11,13 +11,30 @@ import main.java.com.gerenciador.Tarefa;
 class TarefaTest {
 
 	@Test
-	void testCriacao() {
+	void testCriacaoTarefa() {
 		Tarefa tarefa = new Tarefa("Teste", "descricaoTeste", LocalDate.now(), "alta");
-		
+
 		assertEquals("Teste", tarefa.getTitulo());
 		assertEquals("descricaoTeste", tarefa.getDescricao());
 		assertEquals(LocalDate.now(), tarefa.getDataDeVencimento());
 		assertEquals("alta", tarefa.getPrioridade());
+	}
+
+	@Test
+	void testEdicaoTarefa() {
+		Tarefa tarefa = new Tarefa("Teste", "descricaoTeste", LocalDate.now(), "alta");
+
+		assertEquals("Teste", tarefa.getTitulo());
+		assertEquals("descricaoTeste", tarefa.getDescricao());
+		assertEquals(LocalDate.now(), tarefa.getDataDeVencimento());
+		assertEquals("alta", tarefa.getPrioridade());
+		
+		tarefa.atualizaDetalhes("Fazer Atividade", "realizar atividade", LocalDate.of(2023, 8, 1), "media");
+
+		assertEquals("Fazer Atividade", tarefa.getTitulo());
+		assertEquals("realizar atividade", tarefa.getDescricao());
+		assertEquals(LocalDate.of(2023, 8, 1), tarefa.getDataDeVencimento());
+		assertEquals("media", tarefa.getPrioridade());
 	}
 
 }
