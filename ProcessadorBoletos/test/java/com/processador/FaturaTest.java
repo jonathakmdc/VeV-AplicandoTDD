06cd32534f2ada6d444e910d0df744d6aaaa6405
GustavoNeery <gustavoalvesneryy@hotmail.com>
2023-08-21 19:18:35 -0300

    correção do nome da classe

--- ProcessadorBoletos/test/java/com/processador/FaturaTest.java
+++ ProcessadorBoletos/test/java/com/processador/FaturaTest.java
@@ -4,11 +4,10 @@ import ProcessadorBoletos.src.main.java.com.processador.Fatura;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 
-class FaturaTeste {
+class FaturaTest {
 
     @Test
     void testCriacao() {
