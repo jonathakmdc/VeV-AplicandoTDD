cb487a7cb79c52148a1aa4dba6df25e233f78e53
jonathakmdc <jonatha.kennedy.costa@ccc.ufcg.edu.br>
2023-08-20 23:24:06 -0300

    ajuste para passar no teste de adição de tarefas no gerenciador

--- GerenciadorTarefas/src/main/java/com/gerenciador/GerenciadorDeTarefas.java
+++ GerenciadorTarefas/src/main/java/com/gerenciador/GerenciadorDeTarefas.java
@@ -1,5 +1,23 @@
 package main.java.com.gerenciador;
 
-public class GerenciadorDeTarefas {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class GerenciadorDeTarefas {
+	private List<Tarefa> tarefas;
+
+	public GerenciadorDeTarefas() {
+		this.tarefas = new ArrayList();
+	}
+
+	public void addTarefa(String titulo, String descricao, LocalDate dataDeVencimento, String prioridade) {
+		Tarefa tarefa = new Tarefa(titulo, descricao, dataDeVencimento, prioridade);
+		
+		this.tarefas.add(tarefa);
+	}
+
+	public int getQuantidadeTarefas() {
+		return this.tarefas.size();
+	}
 }
