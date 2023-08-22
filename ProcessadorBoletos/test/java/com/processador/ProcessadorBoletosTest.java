8486c9cf05d92f38067b60d56d5af268270ca7c3
GustavoNeery <gustavoalvesneryy@hotmail.com>
2023-08-21 23:18:18 -0300

    teste verifica se fatura foi paga e se foram criados 3 boletos

--- ProcessadorBoletos/test/java/com/processador/ProcessadorBoletosTest.java
+++ ProcessadorBoletos/test/java/com/processador/ProcessadorBoletosTest.java
@@ -1,6 +1,7 @@
 package ProcessadorBoletos.test.java.com.processador;
 
 import ProcessadorBoletos.src.main.java.com.processador.Boleto;
+import ProcessadorBoletos.src.main.java.com.processador.Fatura;
 import ProcessadorBoletos.src.main.java.com.processador.Pagamento;
 import ProcessadorBoletos.src.main.java.com.processador.ProcessadorDeBoletos;
 import org.junit.jupiter.api.Test;
@@ -8,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ProcessadorBoletosTest {
 
@@ -52,5 +53,36 @@ public class ProcessadorBoletosTest {
 
         ProcessadorDeBoletos processadorDeBoletos = new ProcessadorDeBoletos();
         assertEquals(1200.00, processadorDeBoletos.somaBoletosPagos(listaBoletos), 2);
+    }
+
+    @Test
+    void testFaturaPaga() {
+        Boleto boleto1 = new Boleto("112233", LocalDate.now(), 500);
+        Boleto boleto2 = new Boleto("445566", LocalDate.now(), 500);
+        Boleto boleto3 = new Boleto("778899", LocalDate.now(), 200);
+        Fatura fatura = new Fatura(LocalDate.now(), 1500.00, "Carlos");
+        Fatura fatura2 = new Fatura(LocalDate.now(), 1000.00, "Carlos");
+
+        ArrayList<Boleto> listaBoletos = new ArrayList<Boleto>();
+        ArrayList<Pagamento> pagamentos = new ArrayList<Pagamento>();
+
+        listaBoletos.add(boleto1);
+        listaBoletos.add(boleto2);
+        listaBoletos.add(boleto3);
+
+        Pagamento pagamento1 = new Pagamento(boleto1.getValorPago(), boleto1.getData(),"BOLETO");
+        Pagamento pagamento2 = new Pagamento(boleto2.getValorPago(), boleto2.getData(),"BOLETO");
+        Pagamento pagamento3 = new Pagamento(boleto3.getValorPago(), boleto3.getData(),"BOLETO");
+
+        pagamentos.add(pagamento1);
+        pagamentos.add(pagamento2);
+        pagamentos.add(pagamento3);
+
+        ProcessadorDeBoletos processadorDeBoletos = new ProcessadorDeBoletos();
+        assertFalse(processadorDeBoletos.verificaFaturaPaga(processadorDeBoletos.somaBoletosPagos(listaBoletos), fatura.getValorTotal()));
+        assertEquals(pagamentos, processadorDeBoletos.analisaBoletos(listaBoletos));
+
+        assertTrue(processadorDeBoletos.verificaFaturaPaga(processadorDeBoletos.somaBoletosPagos(listaBoletos), fatura2.getValorTotal()));
+        assertEquals(pagamentos, processadorDeBoletos.analisaBoletos(listaBoletos));
     }
 }
