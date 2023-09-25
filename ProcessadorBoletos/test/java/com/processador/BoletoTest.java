045b3f38280d9300965442cac8afd5aa5374610e
GustavoNeery <gustavoalvesneryy@hotmail.com>
2023-09-25 17:26:20 -0300

    refactor tests using junit5

--- ProcessadorBoletos/test/java/com/processador/BoletoTest.java
+++ ProcessadorBoletos/test/java/com/processador/BoletoTest.java
@@ -1,15 +1,19 @@
 package ProcessadorBoletos.test.java.com.processador;
 
 import ProcessadorBoletos.src.main.java.com.processador.Boleto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Testar a funcionalidade de boleto")
 public class BoletoTest {
-
+	
     @Test
+    @DisplayName("Teste para criação de boletos")
     void testCriacao() {
         Boleto boleto = new Boleto("112233", LocalDate.now(), 1500.0);
 
--- ProcessadorBoletos/test/java/com/processador/PagamentoTest.java
+++ ProcessadorBoletos/test/java/com/processador/PagamentoTest.java
@@ -1,15 +1,18 @@
 package ProcessadorBoletos.test.java.com.processador;
 
 import ProcessadorBoletos.src.main.java.com.processador.Pagamento;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Testar a funcionalidade de pagamento")
 public class PagamentoTest {
 
     @Test
+    @DisplayName("Teste para criação de pagamento")
     void testCriacao() {
         Pagamento pagamento = new Pagamento(1500.00, LocalDate.now(),"BOLETO");
 
